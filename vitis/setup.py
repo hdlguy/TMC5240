df18a8af6ae6a7faa072c188b485360c2974d7bc
hdlguy <hdlguy@hdlguy.com>
2026-05-04 19:40:25 -0400

    changed setup.py to use portable calls to remove workspace folder and make symbolic links

--- vitis/setup.py
+++ vitis/setup.py
@@ -4,8 +4,11 @@
 import vitis
 import os
 import sys
+import shutil
+from pathlib import Path
 
-app_name = sys.argv[1]
+#app_name = sys.argv[1]
+app_name = "tmc5240_test"
 #app_name = "hello1"
 print("app_name = ", app_name, "\n")
 
@@ -15,7 +18,8 @@ cpu_name = "microblaze_0"
 #cpu_name = "ps7_cortexa9_0"
 #cpu_name = "psu_cortexa53_0"
 
-os.system('rm -rf workspace')
+#os.system('rm -rf workspace')
+shutil.rmtree('workspace')
 
 client = vitis.create_client()
 client.set_workspace(path="workspace")
@@ -46,13 +50,14 @@ comp = client.create_app_component(
 )
 
 # add source files as symbolic links
-os.system(f"ln -s ../../../src/{app_name}/test.c workspace/{app_name}/src/test.c")
-os.system(f"ln -s ../../../src/fpga.h workspace/{app_name}/src/fpga.h")
-os.system(f"ln -s ../../../src/tmc5240_spi.h workspace/{app_name}/src/tmc5240_spi.h")
-# TMC5240.c  TMC5240.h  TMC5240_HW_Abstraction.h
-os.system(f"ln -s ../../../src/TMC5240/TMC5240_HW_Abstraction.h workspace/{app_name}/src/TMC5240_HW_Abstraction.h")
-os.system(f"ln -s ../../../src/TMC5240/TMC5240.h                workspace/{app_name}/src/TMC5240.h")
-os.system(f"ln -s ../../../src/TMC5240/TMC5240.c                workspace/{app_name}/src/TMC5240.c")
+#os.system(f"ln -s ../../../src/{app_name}/test.c                    workspace/{app_name}/src/test.c")
+#os.system(f"ln -s ../../../src/fpga.h                               workspace/{app_name}/src/fpga.h")
+#os.system(f"ln -s ../../../src/tmc5240_spi.h                        workspace/{app_name}/src/tmc5240_spi.h")
+#os.system(f"ln -s ../../../src/TMC5240/TMC5240_HW_Abstraction.h     workspace/{app_name}/src/TMC5240_HW_Abstraction.h")
+target=Path("src/tmc5240_test/test.c").resolve();        link=Path("workspace/tmc5240_test/src/test.c");                      link.symlink_to(target)            
+target=Path("src/fpga.h").resolve();                     link=Path("workspace/tmc5240_test/src/fpga.h");                      link.symlink_to(target)            
+target=Path("src/tmc5240_spi.h").resolve();              link=Path("workspace/tmc5240_test/src/tmc5240_spi.h");               link.symlink_to(target)            
+target=Path("src/TMC5240_HW_Abstraction.h").resolve();   link=Path("workspace/tmc5240_test/src/TMC5240_HW_Abstraction.h");    link.symlink_to(target)            
 
 status = comp.clean()
 status = platform.build()
